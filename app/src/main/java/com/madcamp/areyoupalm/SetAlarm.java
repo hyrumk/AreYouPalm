8a2441551519370f553ed02faf67e27f2a50d47d
Park Sang June <67790478+SangJune0627@users.noreply.github.com>
2021-01-23 21:56:33 +0900

    cancel button work

--- app/src/main/java/com/madcamp/areyoupalm/SetAlarm.java
+++ app/src/main/java/com/madcamp/areyoupalm/SetAlarm.java
@@ -2,7 +2,15 @@ package com.madcamp.areyoupalm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class SetAlarm extends AppCompatActivity {
 
@@ -10,5 +18,32 @@ public class SetAlarm extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_alarm);
+
+        ImageButton bt_calendar = (ImageButton) findViewById(R.id.bt_alarm_calender);
+        bt_calendar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar cal = Calendar.getInstance();
+                DatePickerDialog dialog = new DatePickerDialog(getApplicationContext(),
+                        DatePickerDialog.OnDateSetListener{datePicker, y, m, d-> Toast.makeText(this, "$y-$m-$d", Toast.LENGTH_SHORT).show()},
+                        cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH));
+            }
+        });
+
+        Button bt_cancel = (Button) findViewById(R.id.bt_cancel);
+        bt_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        Button bt_save = (Button) findViewById(R.id.bt_save);
+        bt_save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
