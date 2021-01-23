1aa13a0a393d3d8173c0d1c10a0a699ceb21cfcf
hyrumk <hyrumk@kaist.ac.kr>
2021-01-23 19:41:03 +0900

    Try SMS feature

--- app/src/main/java/com/madcamp/areyoupalm/MainActivity.java
+++ app/src/main/java/com/madcamp/areyoupalm/MainActivity.java
@@ -1,8 +1,12 @@
 package com.madcamp.areyoupalm;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
@@ -13,7 +17,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
 
         ImageButton bt_addAlarm = (ImageButton)findViewById(R.id.bt_addAlarm);
         bt_addAlarm.setOnClickListener(new View.OnClickListener() {
