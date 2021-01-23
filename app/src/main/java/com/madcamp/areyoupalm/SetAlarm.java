64d21103f7afacc78dca8f1b2bf159732827302f
Park Sang June <67790478+SangJune0627@users.noreply.github.com>
2021-01-23 22:53:58 +0900

    중간 푸쉬

--- app/src/main/java/com/madcamp/areyoupalm/SetAlarm.java
+++ app/src/main/java/com/madcamp/areyoupalm/SetAlarm.java
@@ -3,7 +3,9 @@ package com.madcamp.areyoupalm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Debug;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
@@ -14,19 +16,36 @@ import java.util.Calendar;
 
 public class SetAlarm extends AppCompatActivity {
 
+    int alarm_year;
+    int alarm_month;
+    int alarm_date;
+    int alarm_hour;
+    int alarm_minute;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_alarm);
 
+        Intent intent  = getIntent();
+        boolean isModifying= intent.getBooleanExtra("ismodifying",false);
+        if(isModifying){
+            System.out.println("ㅇㅇㅇㅇ");
+        }
+
         ImageButton bt_calendar = (ImageButton) findViewById(R.id.bt_alarm_calender);
         bt_calendar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 Calendar cal = Calendar.getInstance();
-                DatePickerDialog dialog = new DatePickerDialog(getApplicationContext(),
-                        DatePickerDialog.OnDateSetListener{datePicker, y, m, d-> Toast.makeText(this, "$y-$m-$d", Toast.LENGTH_SHORT).show()},
-                        cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH));
+                DatePickerDialog dialog = new DatePickerDialog(SetAlarm.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int month, int date) {
+                        setDate(year,month,date);
+                    }
+                }, cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DATE));
+                dialog.show();
             }
         });
 
@@ -45,5 +64,11 @@ public class SetAlarm extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    public void setDate(int year,int month, int date){
+        alarm_year = year;
+        alarm_month = month;
+        alarm_date = date;
     }
 }
