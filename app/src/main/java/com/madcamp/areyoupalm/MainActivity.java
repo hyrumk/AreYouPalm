f6f3778e1888949a84f75c8bc63aa09d15687073
Park Sang June <67790478+SangJune0627@users.noreply.github.com>
2021-01-27 17:57:28 +0900

    s

--- app/src/main/java/com/madcamp/areyoupalm/MainActivity.java
+++ app/src/main/java/com/madcamp/areyoupalm/MainActivity.java
@@ -8,6 +8,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
@@ -16,6 +17,8 @@ import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,6 +37,9 @@ public class MainActivity extends AppCompatActivity {
         recyclerView = (RecyclerView) findViewById(R.id.rcv_alarms);
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
+
+//        SharedPreferences sf = getSharedPreferences("alarmList",MODE_PRIVATE);
+
         AlarmListApp alarmListApp = (AlarmListApp) getApplication();
         adapter = new RecyclerViewAdapter(alarmListApp.getAlarmList());
         recyclerView.setAdapter(adapter);
